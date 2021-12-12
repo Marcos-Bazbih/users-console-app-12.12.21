a648d616689171f3e1d1124304fc7609e459358c
Marcos Bazbih <marcosbaza100@gmail.com>
2021-12-12 20:29:39 +0200

    User class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using UsersConsoleApp;
+
+List<User> users = new List<User>();
+
+void GetFourUsers()
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        User userToAdd = new User();
+
+        Console.WriteLine("Enter first name");
+        userToAdd.FirstName = Console.ReadLine();
+
+        Console.WriteLine("Enter last name");
+        userToAdd.LastName = Console.ReadLine();
+
+        Console.WriteLine("Enter birth year");
+        userToAdd.BirthYear = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter email");
+        userToAdd.Email = Console.ReadLine();
+
+        users.Add(userToAdd);
+
+    }
+
+    foreach (User item in users)
+    {
+        item.PrintUserInfo();
+    }
+
+    users.Sort();
+
+    foreach (User item in users)
+    {
+        item.PrintUserInfo();
+    }
+
+}
+
+GetFourUsers();
---
+++ User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersConsoleApp
+{
+    internal class User : IComparable
+    {
+        string firstName;
+        string lastName;
+        int birthYear;
+        string email;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+        public int BirthYear
+        {
+            get { return this.birthYear; }
+            set { this.birthYear = value; }
+        }
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+
+        public User() { }
+
+        public void PrintUserInfo()
+        {
+            Console.WriteLine($"Name: {this.FirstName} {this.LastName} | Birth Year: {this.BirthYear} | Email: {this.Email}");
+        }
+        public int CompareTo(object obj)
+        {
+            User u = (User)obj;
+            if (this.BirthYear < u.BirthYear) return -1;
+            if (this.BirthYear > u.BirthYear) return 1;
+            return 0;
+        }
+
+    }
+}
